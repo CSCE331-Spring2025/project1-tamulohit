f49ce0cdd6b16cdb3cede40d8bf14e9b26849319
Lohitaksh Allampalli <lohit@tamu.edu>
2025-02-01 19:57:05 -0600

    added the button in all pages and JS style switch functionality

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleStyle(){
+    let theme = document.getElementById("style");
+
+    // let currTheme = theme.getAttribute("href");
+    let newTheme = "style1.css"; // default to style1
+    if (theme.getAttribute("href") === "style1.css"){ 
+        newTheme = "style2.css";
+    } 
+    else{
+        newTheme = "style1.css";
+    }
+
+    theme.setAttribute("href", newTheme);
+    localStorage.setItem("currTheme", newTheme);
+}
+
+window.onload = function(){
+    let savedTheme = localStorage.getItem("currTheme") || "style1.css";
+    document.getElementById("style").setAttribute("href", savedTheme);
+}
